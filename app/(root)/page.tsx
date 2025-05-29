3aa0addea2a9ffd5500bf0273d622a65092efd66
LabSakshi <sakshigupta0513@gmail.com>
2025-05-29 17:56:14 +1000

    feat: updated configuration for tailwind and navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import SearchForm from "../components/SearchForm";
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Home</h1>
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          {" "}
+          <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Idea, Vote for Pitches, and Get NOticed in Virtual Competitions
+        </p>
+        <SearchForm />
+      </section>
+    </>
   );
 };
 
